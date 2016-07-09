b89370baba4fd6d45d1c5205ad313fee03cc6119
Adam Parrish <aparrish@neosavvy.com>
2016-07-09 15:33:19 -0400

    Initial

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,14 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I can though"
+  [& args]
+  (println "I am a little teapot!"))
+(println "Cleanliness is next to godliness")
+
+(defn train
+  []
+  (println "Choo Choo!"))
+
+(+ 1 (* 2) 3 4)
