bf08fbe0bacd959c19b4ac1b56052b6d2ee9b06b
Adam Parrish <aparrish@neosavvy.com>
2017-07-15 09:40:15 +0900

    Preparing for hobbit violence.

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -298,3 +298,33 @@
 (re-find #"^left-" "cleft-chin")
 (re-find #"^left-" "wonglebart")
 
+(matching-part {:name "left-eye" :size 1})
+(matching-part {:name "head" :size 1})
+
+
+;; reduce
+(reduce + [1 2 3 4]) ;; with no initial value ie 0
+(reduce + 15 [1 2 3 4]) ;; with initial value 15
+
+(defn my-reduce
+  ([f initial coll]
+   (loop [result initial remaining coll]
+     (if (empty? remaining )
+       result
+       (recur (f result (first remaining)) (rest remaining)))))
+  ([f [head & tail]]
+   (my-reduce f head tail)))
+
+(my-reduce + 15 [1 2 3 4])
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts
+          ))
+(better-symmetrize-body-parts asym-hobbit-body-parts)
+
+
